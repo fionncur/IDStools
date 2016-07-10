687b7af9db8b0bf84f46cc40d681b44ba415165f
Thomas Jonsson <johnso@kth.se>
2016-07-10 15:21:30 +0200

    added new variables, taken from the EU-ITM GGD

--- src/ids_grid_common.f90
+++ src/ids_grid_common.f90
@@ -4,6 +4,28 @@ module ids_grid_common
 
   implicit none
 
+  integer, parameter :: GRID_UNDEFINED = 0
+
+  ! Data representation definitions
+  integer, parameter :: GEO_TYPE_STANDARD = 1
+  character(*), parameter :: GEO_TYPE_ID_STANDARD = "Standard"
+  integer, parameter :: GEO_TYPE_FOURIER = 2
+  character(*), parameter :: GEO_TYPE_ID_FOURIER = "Fourier"
+
+ ! Field aligned vector definitions
+  integer, parameter :: VEC_ALIGN_DEFAULT = 1
+  character(len=132), parameter :: VEC_ALIGN_DEFAULT_ID = "DEFAULT"
+
+  integer, parameter :: VEC_ALIGN_POLOIDAL = 1001
+  character(len=132), parameter :: VEC_ALIGN_POLOIDAL_ID = "Poloidal"
+  integer, parameter :: VEC_ALIGN_RADIAL = 1002
+  character(len=132), parameter :: VEC_ALIGN_RADIAL_ID = "Radial"
+  integer, parameter :: VEC_ALIGN_PARALLEL = 1003
+  character(len=132), parameter :: VEC_ALIGN_PARALLEL_ID = "Parallel"
+
+  integer, parameter :: VEC_ALIGN_TOROIDAL = 1004
+  character(len=132), parameter :: VEC_ALIGN_TOROIDAL_ID = "Toroidal"
+
   !> unspecified
   integer, parameter :: COORDTYPE_unspecified = 0
 
