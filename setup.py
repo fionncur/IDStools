45470234b68d5bdd28a13ef2ca3686b331b046bd
Bartek Palak <bartek@man.poznan.pl>
2017-02-09 15:11:07 +0100

    Changes in setup.py to fit IMAS naming conventions

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 
 from numpy.distutils.core import setup
 
-setup(name='IMAS ISIP Tools',
+setup(name='IMAS IDS Tools',
       version='1.0',
-      description='IMAS ISIP Python tools',
+      description='IMAS IDS Python tools',
       packages=['imastools'],
       py_modules=[],
       scripts=['bin/imasdbs', 'bin/idsdiff', 'bin/idsdump', 'bin/idscopy']
