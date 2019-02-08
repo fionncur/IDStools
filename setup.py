1b6f420fd2cd21bc40924367e0038c972cf61058
Louwrens van Dellen <louwrens.vandellen@iter.org>
2019-02-08 22:46:36 +0100

    Prefer setuptools, add version discovery in setup.py, simplify a bit. (IMAS-1024)

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
 
-from numpy.distutils.core import setup
-import os
+from setuptools import setup
+import os,glob
+import subprocess
 
 # Generate list of python scripts
-here = os.path.abspath(os.path.dirname(__file__))
-script_files = []
-for file in os.listdir(os.path.join(here,'bin')):
-    script_files.append(os.path.join(here,'bin',file))
+script_files = glob.glob("bin/*")
 
+# Get version by PKGVERSION, .version file, or git describe
+def get_version():
+    version = os.getenv("PKGVERSION")
+    if not version and os.path.isfile('.version'):
+        version = open(".version").read()
+    if not version and os.path.isdir('.git'):
+        version = subprocess.check_output(["git", "describe"]).strip().decode('ascii')
+        if '-' in version:
+            p = version.split('-')
+            version = p[0]+'.dev'+p[1]+'+'+''.join(p[2:])
+    return version
+        
 setup(name='IMAS IDS Tools',
-      version       = os.getenv("PKGVERSION"),
+      version       = get_version(),
       description   = 'IMAS IDS Python tools',
       author        = "ITER Organization",
       author_email  = "imas-support@iter.org",
