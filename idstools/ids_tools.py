fionncur/IDStools

commit f13f0de5d67c2e2d77e8f3213475809886720ff6

Bugfix idstools/ids_tools.py for python 3. (IMAS-1024)

idstools/ids_tools.py
@@ -117,14 +117,13 @@ class ImasDb():
         import types
 
         # FIXME: Crude hack to get names of all time-dependent IDS. Will be fixed with improved UAL interface
-        timedep_ids_test = lambda x: isinstance(x, types.InstanceType)
-        timedep_idss = inspect.getmembers(self.db, timedep_ids_test )
+        timedep_idss = inspect.getmembers(self.db)
         #print("IDSS:  ", timedep_idss)
 
         result = []
         for idsnameArray, obj in timedep_idss:
             #print('X',idsnameArray,'Y',obj)
-            for occurrence in xrange(4):
+            for occurrence in range(4):
                 if occurrence == 0:
                     idsname = idsnameArray
                 else:

commit 6da753513dc31f536c30dbbc0af23b9eabca8421

Correcting error introduced in f13f0de5d67 of IMAS-1024 [IMAS-2233]

idstools/ids_tools.py
@@ -117,8 +117,9 @@ class ImasDb():
         import types
 
         # FIXME: Crude hack to get names of all time-dependent IDS. Will be fixed with improved UAL interface
-        timedep_idss = inspect.getmembers(self.db)
-        #print("IDSS:  ", timedep_idss)
+        timedep_ids_test = lambda x: isinstance(x, types.InstanceType)
+        timedep_idss = inspect.getmembers(self.db, timedep_ids_test )        
+        print("IDSS:  ", timedep_idss)
 
         result = []
         for idsnameArray, obj in timedep_idss: