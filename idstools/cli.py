02d13a06862ccab6c8e728f655b40dc437381d6f
Olivier Hoenen <Olivier.Hoenen@iter.org>
2020-11-09 16:00:19 +0100

    Do not keep deprecated args in shared parser

--- idstools/cli.py
+++ idstools/cli.py
@@ -4,16 +4,13 @@ from imas import imasdef
 
 # default parent parser for all idstools scripts
 imas_parser = argparse.ArgumentParser(add_help=False)
-imas_parser.add_argument("-u", "--user", 
-                         type=str, default=os.environ["USER"],
+imas_parser.add_argument("-u", "--user_or_path", dest="user", 
+                         type=str, default="public", #os.environ["USER"],
                          help="user \t\t(default=%(default)s)")
 db_group = imas_parser.add_mutually_exclusive_group()
 db_group.add_argument("--database", "-d", 
-                   type=str, default="iter",
-                   help="database name \t(default=%(default)s)")
-db_group.add_argument("--tokamak", "-t", 
-                   type=str, dest="database", default=os.getenv("TOKAMAKNAME"),
-                   help="tokamak name \t[deprecated: use -d instead]")
+                      type=str, default="iter",
+                      help="database name \t(default=%(default)s)")
 imas_parser.add_argument("--backend", "-b", 
                          type=str, default="MDSPLUS", 
                          help="backend format \t(default=%(default)s)")
