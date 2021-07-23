0c6de17a53c131319664cf143473c8247c6c3ef9
Olivier Hoenen <Olivier.Hoenen@iter.org>
2021-07-23 10:18:32 +0200

    Add ids_rescale_eq.py into the list of scripts to put in the PATH

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import subprocess
 script_files = glob.glob("bin/*")
 script_files.append("database_tools/ids_shift_eq.py")
 script_files.append("database_tools/idstest_dim2d.py")
-
+script_files.append("database_tools/ids_rescale_eq.py")
 
 # Get version by PKGVERSION, .version file, or git describe
 def get_version():
