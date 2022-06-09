be7bace666140af71f06b6656b37f3c891b49999
Marc Vidal <marc.vidal@iter.org>
2022-06-09 19:01:22 +0200

    Incorporates backend selection, enhances argsparse interaction (user, database, version, idspath, backend options), desired value return almost completed (WIP)

--- database_tools/ids_extractor.py
+++ database_tools/ids_extractor.py
@@ -1,50 +1,71 @@
 # From Rosetta to IDS to readable CSV
+import imas
+from imas import imasdef
 import pandas as pd
 import argparse
 from pathlib import Path
+from idstools.cli import get_backend_id
 
 parser = argparse.ArgumentParser(description="This script applies conversion of IDS data into readable CSV files.")
-parser.add_argument("--path", type=str, default="/home/ITER/hoeneno/public/imasdb/benchmark/3", help="Path to database containing desired shots and runs")
-# parser.add_argument("--backend", type=str, default="HDF5", help="Desired backend of use (currently supports MDS+ or HDF5")
+parser.add_argument("--user", type=str, default="hoeneno", help="User harboring desired data")
+parser.add_argument("--database", type=str, default="benchmark", help="Database harboring desired data")
+parser.add_argument("--version", type=str, default="3", help="Version of IMAS database")
+parser.add_argument("--idspath", type=str, default="summary/global_quantities/ip/value", help="IDS path to desired data to be collected ")
+parser.add_argument("--backend", type=str, default="HDF5", help="Desired backend of use (will read MDSPLUS, HDF5, or both")
 args = parser.parse_args()
-path = args.path
-# backend = args.backend
-
+database = args.database
+idspath = args.idspath
+backend = args.backend.upper()
+locpath = "/home/ITER/" + args.user + "/public/imasdb/" + database + "/" + args.version
 shots = []
 runs = []
+values = []
 backends = []
+
+idsname = idspath.split('/')[0]
+valpath = idspath.split('/')[1:]
+
 
 # MDS+
 
-folder = Path(path).glob('**/*.datafile')
-for entry in folder:
-    file = str(entry).split('/')[-1].split('_')[1].split('.')[0]
-    if len(file) <= 4:
-        shot = 0
-    else:
-        shot = int(file[0:-4])
-    run = int(file[-4:])+10000*int(str(entry).split('/')[-2])
-    shots.append(shot)
-    runs.append(run)
-    backends.append('MDSPLUS')
+if backend == 'MDSPLUS' or backend == 'MDS+' or backend == backend == "BOTH":
+    folder = Path(locpath).glob('**/*.datafile')
+    for entry in folder:
+        file = str(entry).split('/')[-1].split('_')[1].split('.')[0]
+        if len(file) <= 4:
+            shot = 0
+        else:
+            shot = int(file[0:-4])
+        run = int(file[-4:])+10000*int(str(entry).split('/')[-2])
+        de = imas.DBEntry(get_backend_id(args.backend), database, shot, run)
+        de.open()
+        value = de.partial_get(idsname, valpath)
+        shots.append(shot)
+        runs.append(run)
+        values.append(value)
+        de.close()
+        backends.append('MDSPLUS')
 
 
 # HDF5
 
-folder = Path(path).glob('**/*master.h5')
-for entry in folder:
-    shot = int(str(entry).split('/')[-3])
-    run = int(str(entry).split('/')[-2])
-    shots.append(shot)
-    runs.append(run)
-    backends.append('HDF5')
+if backend == 'HDF5' or backend == "BOTH":
+    folder = Path(locpath).glob('**/*master.h5')
+    for entry in folder:
+        shot = int(str(entry).split('/')[-3])
+        run = int(str(entry).split('/')[-2])
+        shots.append(shot)
+        runs.append(run)
+        print(str(get_backend_id(args.backend)) + ' ' + str(database) + ' ' + str(shot) + ' ' + str(run))
+        de = imas.DBEntry(get_backend_id(args.backend), database, shot, run)
+        de.open()
+        value = de.partial_get(idsname, valpath)
+        values.append(value)
+        de.close()
+        backends.append('HDF5')
 
 
-df = pd.DataFrame(list(zip(shots, runs, backends)), columns=['SHOT', 'RUN', 'BACKEND'])
+df = pd.DataFrame(list(zip(shots, runs, backends)), columns=['SHOT', 'RUN', 'VALUE', 'BACKEND'])
 print(df)
 
-
 # df.to_csv(r'C:\users\vidalm\Work Folders\Desktop\export_dataframe.csv', index=False, header=True)
-
-
-# Path to folder, I need to explore in this folder if I can find and list all available shot and run numbers of IDS entries for given backend
