bce7d718008d651e21c5d9f616743e549cbfe6dd
Marc Vidal <marc.vidal@iter.org>
2022-06-12 11:48:30 +0200

    Implements Pulse and Run number retrieval as functions, error handling, enhanced argsparse, pulse and run as tuples (due to interdependency) and saving values as tuples for faster execution

--- database_tools/ids_extractor.py
+++ database_tools/ids_extractor.py
@@ -1,71 +1,109 @@
-# From Rosetta to IDS to readable CSV
+# From IDS to readable CSV
 import imas
+import os
 from imas import imasdef
 import pandas as pd
 import argparse
 from pathlib import Path
 from idstools.cli import get_backend_id
-
-parser = argparse.ArgumentParser(description="This script applies conversion of IDS data into readable CSV files.")
-parser.add_argument("--user", type=str, default="hoeneno", help="User harboring desired data")
-parser.add_argument("--database", type=str, default="benchmark", help="Database harboring desired data")
-parser.add_argument("--version", type=str, default="3", help="Version of IMAS database")
-parser.add_argument("--idspath", type=str, default="summary/global_quantities/ip/value", help="IDS path to desired data to be collected ")
-parser.add_argument("--backend", type=str, default="HDF5", help="Desired backend of use (will read MDSPLUS, HDF5, or both")
-args = parser.parse_args()
-database = args.database
-idspath = args.idspath
-backend = args.backend.upper()
-locpath = "/home/ITER/" + args.user + "/public/imasdb/" + database + "/" + args.version
-shots = []
-runs = []
-values = []
-backends = []
-
-idsname = idspath.split('/')[0]
-valpath = idspath.split('/')[1:]
+progbar = True
+try:
+    from tqdm import tqdm
+except ModuleNotFoundError:
+    print(f"Install tqdm to enable progress bar")
+    progbar = False
 
 
-# MDS+
+# Function that lists Pulse and Run numbers from given database, in MDSPLUS
 
-if backend == 'MDSPLUS' or backend == 'MDS+' or backend == backend == "BOTH":
+def mdsListPulseRun(locpath):
+    if not locpath.exists():
+        print("No such database file or directory. Please check spelling.")
+        exit()
+    pulses = []
     folder = Path(locpath).glob('**/*.datafile')
     for entry in folder:
         file = str(entry).split('/')[-1].split('_')[1].split('.')[0]
         if len(file) <= 4:
-            shot = 0
+            pulse = 0
         else:
-            shot = int(file[0:-4])
-        run = int(file[-4:])+10000*int(str(entry).split('/')[-2])
-        de = imas.DBEntry(get_backend_id(args.backend), database, shot, run)
-        de.open()
-        value = de.partial_get(idsname, valpath)
-        shots.append(shot)
-        runs.append(run)
-        values.append(value)
-        de.close()
-        backends.append('MDSPLUS')
+            pulse = int(file[0:-4])
+        run = int(file[-4:]) + 10000 * int(str(entry).split('/')[-2])
+        pulses.append((pulse, run))
+    return pulses
 
 
-# HDF5
+# Function that lists Pulse and Run numbers from given database, in HDF5
 
-if backend == 'HDF5' or backend == "BOTH":
+def hdf5ListPulseRun(locpath):
+    if locpath.exists():
+        print("No such database file or directory. Please check spelling.")
+        exit()
+    pulses = []
     folder = Path(locpath).glob('**/*master.h5')
     for entry in folder:
-        shot = int(str(entry).split('/')[-3])
+        pulse = int(str(entry).split('/')[-3])
         run = int(str(entry).split('/')[-2])
-        shots.append(shot)
-        runs.append(run)
-        print(str(get_backend_id(args.backend)) + ' ' + str(database) + ' ' + str(shot) + ' ' + str(run))
-        de = imas.DBEntry(get_backend_id(args.backend), database, shot, run)
+        pulses.append((pulse, run))
+    return pulses
+
+
+
+parser = argparse.ArgumentParser(description="This script applies conversion of IDS data into readable CSV files.")
+parser.add_argument("--user", type=str, default="", help="User harboring desired data, or public option")
+parser.add_argument("--database", type=str, default="", help="Database harboring desired data")
+parser.add_argument("--version", type=str, default="3", help="Version of IMAS database")
+parser.add_argument("--idspath", type=str, default="", help="IDS path to desired data to be collected")
+parser.add_argument("--saveas", type=str, default="", help="Path to directory ending with name of file to save retrieved data")
+parser.add_argument("--backend", type=str, default="HDF5", help="Desired backend of use (will read MDSPLUS, HDF5, or both")
+parser.add_argument("--index", type=str, default="True", help="Should index be shown in final .csv file?")
+args = parser.parse_args()
+database = args.database
+idspath = args.idspath
+backend = args.backend.upper()
+
+idsname = idspath.split('/')[0]
+valpath = idspath[1 + len(idsname):]
+if args.user == 'public':
+    locpath = Path(os.environ['IMAS_HOME'] + '/shared/imasdb/' + database + "/" + args.version)
+else:
+    locpath = Path(os.path.expanduser('~' + args.user) + "/public/imasdb/" + database + "/" + args.version)
+
+values = []
+
+if not Path(args.saveas).parent.exists():
+    print("No such file or directory to save the results. Please check spelling")
+    exit()
+
+# Retrieve data in MDSPLUS
+
+if backend == 'MDSPLUS' or backend == 'MDS+' or backend == "BOTH":
+    pulses = mdsListPulseRun(locpath)
+    for entry in tqdm(pulses):
+        # get_backend_id(backend) manually entered. If code wishes to be generalized with get_backend_id(backend), 'MDS+' and 'BOTH' options cannot be used.
+        de = imas.DBEntry(12, database, entry[0], entry[1], args.user, args.version)
         de.open()
         value = de.partial_get(idsname, valpath)
-        values.append(value)
         de.close()
-        backends.append('HDF5')
+        values += value,
+
+# Retrieve data in HDF5
+
+if backend == 'HDF5' or backend == "BOTH":
+    pulses = hdf5ListPulseRun(locpath)
+    for entry in tqdm(pulses):
+        de = imas.DBEntry(13, database, entry[0], entry[1], args.user, args.version)
+        de.open()
+        value = de.partial_get(idsname, valpath)
+        de.close()
+        values += value,
 
 
-df = pd.DataFrame(list(zip(shots, runs, backends)), columns=['SHOT', 'RUN', 'VALUE', 'BACKEND'])
-print(df)
-
-# df.to_csv(r'C:\users\vidalm\Work Folders\Desktop\export_dataframe.csv', index=False, header=True)
+df = pd.DataFrame(pulses, columns=['PULSE', 'RUN'])
+df['VALUE'] = values
+saveresultsin = Path(r'' + args.saveas + '.csv')
+if args.index.upper == 'FALSE' or args.index.upper() == 'NO':
+    index = False
+else:
+    index = True
+df.to_csv(saveresultsin, index=index, header=True)
