547c9e92cf24c19651aff4bf20d827969d247825
Marc Vidal <marc.vidal@iter.org>
2022-06-16 11:13:38 +0200

    Incorporates idsdiff compare function to return IDS and STATUS of copy in different backend

--- database_tools/db_migrator.py
+++ database_tools/db_migrator.py
@@ -3,9 +3,11 @@
 import imas
 from imas import imasdef
 import argparse
+import pandas as pd
 from idstools.cli import *
 from pathlib import Path
-from ids_extractor import mdsListPulseRun, hdf5ListPulseRun, getPath
+from ids_extractor import mdsListPulseRun, hdf5ListPulseRun, getDBPath
+from idstools.idsdiff import compare
 progbar = True
 try:
     from tqdm import tqdm
@@ -13,11 +15,9 @@ except ModuleNotFoundError:
     print(f"Install tqdm to enable progress bar")
     progbar = False
 
+pd.set_option('display.max_rows', 90)
 
-# Next steps: incorporate idschk function that validates ids.
-# If validated then convert to HDF5.
-# Read in HDF5 and incorporate ids_diff (compare) function in HDF5 (put inside loop)
-
+# Next steps: incorporate idschk function that validates ids. If validated then convert to HDF5.
 
 parser = argparse.ArgumentParser(description='Copy all IDSs in given directory into a chosen one', parents=[imas_parser])
 parser.add_argument("-do", "--database_out", type=str, default="migrations", help="Name of destination database")
@@ -25,9 +25,11 @@ parser.add_argument("-bo", "--backend_output", type=str, default="HDF5", help="D
 parser.add_argument('ids', nargs='*', type=str, help='IDSs to copy (leave empty to select all IDSs with default occurrence, or append "/n" to copy a specific occurrence "n")')
 args = parser.parse_args()
 
-locpath = getPath(args.user, args.database, args.version)
+locpath = getDBPath(args.user, args.database, args.version)
 
 backend = get_backend_id(args.backend)
+
+log = []
 
 if backend == imas.imasdef.MDSPLUS_BACKEND:
     files = mdsListPulseRun(locpath)
@@ -37,10 +39,12 @@ elif backend == imas.imasdef.HDF5_BACKEND:
 if args.ids == []:
     args.ids = [ids.value for ids in list(imas.IDSName)]
 
-for ids in tqdm(files) if progbar else files:
-    src = imas.DBEntry(backend, args.database, ids[0], ids[1], args.user)
+for pulse in tqdm(files) if progbar else files:
+    idsinf = []
+    run = pulse[1]
+    src = imas.DBEntry(backend, args.database, pulse[0], run, args.user)
     src.open()
-    dest = imas.DBEntry(get_backend_id(args.backend_output), args.database_out, ids[0], ids[1])
+    dest = imas.DBEntry(get_backend_id(args.backend_output), args.database_out, pulse[0], run)
     dest.create()
     for idsname in args.ids:
         inocc = 0
@@ -53,13 +57,17 @@ for ids in tqdm(files) if progbar else files:
             idsobj.dd_version = os.environ['IMAS_VERSION']
         try:
             if idsobj.ids_properties.homogeneous_time != imas.imasdef.EMPTY_INT:
-                print(f'Copying {idsname}')
                 inocc
                 dest.put(idsobj, occurrence=inocc)
                 idsobj2 = dest.get(idsid[0], occurrence=inocc)
-                # compare
+                idsinf.append((idsname, compare(idsobj, idsobj2, verb=False)))
         except Exception as exc:
             print(str(exc), file=sys.stderr)
+    log.append(idsinf)
 
     src.close()
     dest.close()
+
+df = pd.DataFrame(files, columns=['PULSE', 'RUN'])
+df['IDS STATUS'] = log
+print(df)
