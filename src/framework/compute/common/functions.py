6d84976a73f9c412663efa98a3dba6918babfb91
Sawantdesai Prasad parshuram EXT <Prasadparshuram.Sawantdesai@iter.org>
2022-12-13 16:46:22 +0100

    IMAS-4412 refactored common function

--- src/framework/compute/common/functions.py
+++ src/framework/compute/common/functions.py
@@ -19,7 +19,7 @@ def get_nearest_value_in_array(
         [float]: [nearest time index]
     """
     time_index = abs(time_array - time_of_interest).argmin()
-    return time_array.flat[time_index], time_index
+    return time_index, time_array.flat[time_index]
 
 
 def compute_time_index(time_array: np.ndarray, time_of_interest: float) -> tuple:
@@ -34,38 +34,22 @@ def compute_time_index(time_array: np.ndarray, time_of_interest: float) -> tuple
         [int]: [integer index found for time requested]
         [float]: [nearest time for time requested]
     """
+    time_value = 0
+    time_index = 0
     time_len = len(time_array)
-    if time_len > 1:
-        if time_of_interest >= 0:
-            time_value, time_index = get_nearest_value_in_array(
-                time_array, time_of_interest
-            )
-        else:
-            time_index = int(time_len / 2)
-            time_value = time_array[time_index]
-    else:
-        if time_len > 0:
-            time_value = time_array[0]
-        else:
-            time_value = 0
-        time_index = 0
+    # length of time array is 1
+    if time_len == 1:
+        time_value = time_array[0]
+        return time_index, time_array[0]
+
+    # length of array is bigger than 1 and asking for valid time of interest
+    if time_of_interest >= 0:
+        time_index, time_value = get_nearest_value_in_array(
+            time_array, time_of_interest
+        )
+        return time_index, time_value
+
+    # time of interest is not valid then calculate default
+    time_index = int(time_len / 2)
+    time_value = time_array[time_index]
     return time_index, time_value
-
-
-# print(
-#     "   Time  = "
-#     + "%.2f" % time_value
-#     + " s in range ["
-#     + "%.2f" % time_array[0]
-#     + ","
-#     + "%.2f" % time_array[time_len - 1]
-#     + "] s",
-#     file=sys.stderr,
-# )
-# print("   Index = ", str(time_index), file=sys.stderr)
-# print(
-#     "   Averaged resolution = ",
-#     (time_array[time_len - 1] - time_array[0]) / (time_len - 1),
-#     " s",
-#     file=sys.stderr,
-# )
