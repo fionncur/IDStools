5674cb3bdd7cfbcb6a71ecd0070578a60bacb8b4
Sawantdesai Prasad parshuram EXT <Prasadparshuram.Sawantdesai@iter.org>
2023-04-05 13:22:43 +0200

    IMAS-4633 added db_converter and db_extractor.py as binaries in setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,8 @@ script_files = glob.glob("bin/*")
 script_files.append("database_tools/ids_shift_eq.py")
 script_files.append("database_tools/ids_rescale_eq.py")
 script_files.append("database_tools/rosettacode.py")
+script_files.append("database_tools/db_converter.py")
+script_files.append("database_tools/db_extractor.py")
 script_files.append("idstools/idsdef.py")
 
 # Generate list of data files
