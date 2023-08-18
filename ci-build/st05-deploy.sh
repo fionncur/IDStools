68e1391d257df109698a0dcc17efafd722ab7f79
Sawantdesai Prasad parshuram EXT <Prasadparshuram.Sawantdesai@iter.org>
2023-08-18 16:01:09 +0200

    fix

--- ci-build/st05-deploy.sh
+++ ci-build/st05-deploy.sh
@@ -67,6 +67,20 @@ set -v
 echo "Contents of versioninfo.txt"
 cat ./ci-build/versioninfo.txt
 
+cat >> ./requirements.txt << EOF
+matplotlib
+numpy
+pandas
+cerberus
+pyyaml
+packaging
+pyparsing
+six
+python-dateutil
+fortranformat
+tabulate
+EOF
+
 MODULE_NAME=IDSTools
 COMMITHASH=$(awk -F "=" '/COMMITHASH/ {print $2}' ./ci-build/versioninfo.txt)
 RAWVERSION=$(awk -F "=" '/VERSION/ {print $2}' ./ci-build/versioninfo.txt)
