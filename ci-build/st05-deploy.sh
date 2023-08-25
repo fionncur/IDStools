98d6a6f8fdc9ec348376cffc2269ec54ce0d5302
Sawantdesai Prasad parshuram EXT <Prasadparshuram.Sawantdesai@iter.org>
2023-08-25 10:23:01 +0200

    added log

--- ci-build/st05-deploy.sh
+++ ci-build/st05-deploy.sh
@@ -154,6 +154,9 @@ echo "-----------------------END----------------------------------"
 eb ./ci-build/files/$MODULE_FULL_VERSION -f ${EB_OPTS} ${EB_HTTP_OPTS}
 echo $MODULE_FULL_VERSION "Installed"
 
+echo "check if /work/imas/opt/bamboo_deploy/easybuild/ path is accessible"
+ll /work/imas/opt/bamboo_deploy/easybuild/
+
 echo "replace mnt with /work/imas/opt/ to work internal path on sdcc" 
 # sed -i -- 's/mnt/work\/imas\/opt/g' /work/imas/opt/bamboo_deploy/easybuild/*.*
 find /work/imas/opt/bamboo_deploy/easybuild/ -type f -not -path '*/\.*' -exec sed -i -- 's/mnt/work\/imas\/opt/g' {} +
