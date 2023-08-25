87e9888298a996854c7457408a2ddf065529de69
Sawantdesai Prasad parshuram EXT <Prasadparshuram.Sawantdesai@iter.org>
2023-08-25 08:14:15 +0200

    mnt -> work/imas/opt change path in all files to access correct folder

--- ci-build/st05-deploy.sh
+++ ci-build/st05-deploy.sh
@@ -154,6 +154,9 @@ echo "-----------------------END----------------------------------"
 eb ./ci-build/files/$MODULE_FULL_VERSION -f ${EB_OPTS} ${EB_HTTP_OPTS}
 echo $MODULE_FULL_VERSION "Installed"
 
+echo "replace mnt with /work/imas/opt/ to work internal path on sdcc" 
+sed -i -- 's/mnt/work\/imas\/opt/g' /work/imas/opt/bamboo_deploy/easybuild/*
+
 echo "check available idstools modules"
 module use -p /work/imas/opt/bamboo_deploy/easybuild/modules/all
 module avail -i idstools
