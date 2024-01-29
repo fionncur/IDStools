64d9b1144ce49e9af019931db47fb43131af4b2a
Sawantdesai Prasad parshuram EXT <Prasadparshuram.Sawantdesai@iter.org>
2024-01-29 11:33:38 +0100

    fixed path of the matplotlib custom style

--- idstools/view/common.py
+++ idstools/view/common.py
@@ -27,12 +27,15 @@ import matplotlib.pyplot as plt
 # plt.rcParams["legend.handlelength"] = lhandle
 
 # plt.rcParams["lines.linewidth"] = 1
-
-plt.style.use(
-    r"/home/ITER/sawantp1/git/idstools/idstools/view/styles/scientific.mplstyle"
-)
-
-
+if os.path.exists(os.path.join(sys.prefix, r"/share/styles/scientific.mplstyle")):
+    plt.style.use(
+        os.path.join(sys.prefix, r"/share/styles/scientific.mplstyle")
+    )
+else:
+    current_directory = os.path.abspath(os.path.dirname(__file__))
+    plt.style.use(
+        os.path.join(current_directory, r"styles/scientific.mplstyle")
+    )
 import logging
 import math
 import weakref
