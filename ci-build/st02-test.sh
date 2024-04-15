956468405b6eabefad88e64d3e7c000b71b74a24
Sawantdesai Prasad parshuram EXT <Prasadparshuram.Sawantdesai@iter.org>
2024-04-15 17:01:07 +0200

    access layer 5 2023b test

--- ci-build/st02-test.sh
+++ ci-build/st02-test.sh
@@ -29,25 +29,6 @@ try source ./tests/st01_test_ids_scripts.sh
 try source ./tests/st02_test_db_scripts.sh 
 try source ./tests/st03_test_analysis_scripts.sh 
 
-echo "-------------------------------------------------------------------------"
-echo "Tools testing with testscripts with IMAS Access Layer 5"
-try module purge
-try module unload -f IMAS
-try module load IMAS/3.40.1-5.1.0-foss-2020b
-try module unload -f IDStools
-echo module list | grep IMAS
-
-echo "Check environment for AL5"
-export PYVERSION=$(python3 -c 'import sys; print("%d.%d"% sys.version_info[0:2])')
-echo "PYVERSION :" $PYVERSION
-export PYTHONPATH=$(get_abs_filename "./${PREFIX_DIR}")/lib/python${PYVERSION}/site-packages:${PYTHONPATH}
-echo "PYTHONPATH :" $PYTHONPATH | grep -i idstools
-export PATH=$(get_abs_filename "./${PREFIX_DIR}")/bin:${PATH}
-echo "PATH :" $PATH | grep -i idstools
-
-try source ./tests/st01_test_ids_scripts.sh 
-try source ./tests/st02_test_db_scripts.sh 
-try source ./tests/st03_test_analysis_scripts.sh 
 
 echo "-------------------------------------------------------------------------"
 echo "Tools testing with testscripts with IMAS Access Layer 5"
@@ -69,6 +50,25 @@ try source ./tests/st01_test_ids_scripts.sh
 try source ./tests/st02_test_db_scripts.sh 
 try source ./tests/st03_test_analysis_scripts.sh 
 
+echo "-------------------------------------------------------------------------"
+echo "Tools testing with testscripts with IMAS Access Layer 5"
+try module purge
+try module unload -f IMAS
+try module load IMAS/3.40.1-5.1.0-foss-2020b
+try module unload -f IDStools
+echo module list | grep IMAS
+
+echo "Check environment for AL5"
+export PYVERSION=$(python3 -c 'import sys; print("%d.%d"% sys.version_info[0:2])')
+echo "PYVERSION :" $PYVERSION
+export PYTHONPATH=$(get_abs_filename "./${PREFIX_DIR}")/lib/python${PYVERSION}/site-packages:${PYTHONPATH}
+echo "PYTHONPATH :" $PYTHONPATH | grep -i idstools
+export PATH=$(get_abs_filename "./${PREFIX_DIR}")/bin:${PATH}
+echo "PATH :" $PATH | grep -i idstools
+
+try source ./tests/st01_test_ids_scripts.sh 
+try source ./tests/st02_test_db_scripts.sh 
+try source ./tests/st03_test_analysis_scripts.sh 
 
 echo "Run pytest for functions testing"
 try python3 -m pytest --junit-xml=${PREFIX_DIR}/test_report.xml tests || exit 1
