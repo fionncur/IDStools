92f05cc24ce3de10b1c35f15f4013d2cf083eaa4
Sawantdesai Prasad parshuram EXT <Prasadparshuram.Sawantdesai@iter.org>
2024-04-15 15:50:23 +0200

    added view script and test for 2023b

--- ci-build/st02-test.sh
+++ ci-build/st02-test.sh
@@ -13,7 +13,7 @@ python3 -m pip install --target=dependencies -r requirements.txt
 export PYTHONPATH=$(get_abs_filename "./dependencies"):${PYTHONPATH}
 
 # run tests
-echo "Check environement for AL4"
+echo "Check environment for AL4"
 export PYVERSION=$(python3 -c 'import sys; print("%d.%d"% sys.version_info[0:2])')
 echo "PYVERSION :" $PYVERSION
 export PYTHONPATH=$(get_abs_filename "./${PREFIX_DIR}")/lib/python${PYVERSION}/site-packages:${PYTHONPATH}
@@ -33,10 +33,11 @@ echo "-------------------------------------------------------------------------"
 echo "Tools testing with testscripts with IMAS Access Layer 5"
 try module purge
 try module unload -f IMAS
-try module load IMAS/3.40.1-5.1.0-intel-2020b
+try module load IMAS/3.40.1-5.1.0-foss-2020b
 try module unload -f IDStools
+echo module list | grep IMAS
 
-echo "Check environement for AL5"
+echo "Check environment for AL5"
 export PYVERSION=$(python3 -c 'import sys; print("%d.%d"% sys.version_info[0:2])')
 echo "PYVERSION :" $PYVERSION
 export PYTHONPATH=$(get_abs_filename "./${PREFIX_DIR}")/lib/python${PYVERSION}/site-packages:${PYTHONPATH}
@@ -47,6 +48,27 @@ echo "PATH :" $PATH | grep -i idstools
 try source ./tests/st01_test_ids_scripts.sh 
 try source ./tests/st02_test_db_scripts.sh 
 try source ./tests/st03_test_analysis_scripts.sh 
+
+echo "-------------------------------------------------------------------------"
+echo "Tools testing with testscripts with IMAS Access Layer 5"
+try module purge
+try module unload -f IMAS
+try module load IMAS/3.41.0-4.11.10-foss-2023b
+try module unload -f IDStools
+echo module list | grep IMAS
+
+echo "Check environment for AL5 2023b"
+export PYVERSION=$(python3 -c 'import sys; print("%d.%d"% sys.version_info[0:2])')
+echo "PYVERSION :" $PYVERSION
+export PYTHONPATH=$(get_abs_filename "./${PREFIX_DIR}")/lib/python${PYVERSION}/site-packages:${PYTHONPATH}
+echo "PYTHONPATH :" $PYTHONPATH | grep -i idstools
+export PATH=$(get_abs_filename "./${PREFIX_DIR}")/bin:${PATH}
+echo "PATH :" $PATH | grep -i idstools
+
+try source ./tests/st01_test_ids_scripts.sh 
+try source ./tests/st02_test_db_scripts.sh 
+try source ./tests/st03_test_analysis_scripts.sh 
+
 
 echo "Run pytest for functions testing"
 try python3 -m pytest --junit-xml=${PREFIX_DIR}/test_report.xml tests || exit 1
--- tests/st03_test_analysis_scripts.sh
+++ tests/st03_test_analysis_scripts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Bamboo test execution script for ids manipulation scripts
+# Bamboo test execution script for analysis scripts
 # Execute script from root directory
 source ./tests/st00_common.sh
 # expand aliases
@@ -14,22 +14,37 @@ rm -rf "$LOG_DIR"
 mkdir -p "$LOG_DIR"
 
 SCRIPTS=(
-    # "dbconverter --u public --database TEST -do MYDB -bo MDSPLUS --validate"
-    "dblist -u public -d TEST list" 
-    "dblist -u public -d TEST list -c" 
-    "dblist -u public -d TEST list -M" 
-    "dblist databases" 
-    "dblist dataversions" 
-    "dbperf -d TEST" 
-    "dbscraper \"core_profiles/profiles_1d(0)/electrons/temperature\" --verbose --list-count 2"  
-    "dbselector -d TEST core_profiles --list-count 2" 
-    "dbselector -d TEST summary --list-count 2")
+"viewcoresources -s 130012 -r 105 -d TEST  --save"
+"viewcoretransport -s 92436 -r 850  -d TEST --save" 
+"vieweccomposition -s 134173 -r 2326 -d TEST --save" 
+"viewecray -s 134173 -r 2326 -d TEST --save" 
+"viewedgeprofiles -s 123314 -r 1 --separatix --wall --save"
+"viewequilibrium -s 134174 -r 117 --rho --pfcoils --save"
+"viewfluxes -s 134174 -r  117 -m CLOSEST"
+"viewhcddistributions -s 130012 -r 115 -d TEST --save" 
+"viewhcdplots -ech 134173/101/public/MDSPLUS/TEST/3 -nbi 130012/115/public/MDSPLUS/TEST/3 -fus 130012/115/public/MDSPLUS/TEST/3 -icrh 130012/15/public/MDSPLUS/TEST/3 --save"
+"viewhcdwaves -s 134173 -r 101 -d TEST --save"
+"viewkineticprofiles -s 134174 -r 117 --save"
+"viewmachinedescription plot wall pf_active --save"
+"viewmachinedescription list pf_active --checkValidity" 
+"viewmachinedescription list pf_active --obsolete" 
+"viewmachinedescription plot wall --save" 
+"viewneutron -s 121014 -r 11 -t 450 --save"
+"viewplasmacompo -s 131047 -r 4"
+"viewpressure -s 134174 -r 117 --save"
+"viewrotation -s 134174 -r 117 --save"
+"viewscenario -s 134174 -r 117 --time 60 --save" 
+"viewscenario -s 134174 -r 117 --noProfiles --save" 
+"viewsources -s 134174 -r  117" 
+"viewspectrometry -s 134000 -r 37 -d TEST --save" 
+"viewwall --save wall iter")
+
 
 execute_scripts "${SCRIPTS[@]}"
 STATUS=$?
 if [[ "$(uname -n)" == *"bamboo"* ]]; then
-    if [ "$STATUS" -ne 0 ]; then
-        exit "$STATUS"
-    fi
+    exit "$STATUS"
 fi
+
+
 
