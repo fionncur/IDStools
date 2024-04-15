66d911479e081912970ac2a7ead9a4bd623aef26
Sawantdesai Prasad parshuram EXT <Prasadparshuram.Sawantdesai@iter.org>
2024-04-15 15:16:51 +0200

    fixed exit 1 not executing commands further

--- ci-build/st02-test.sh
+++ ci-build/st02-test.sh
@@ -22,10 +22,12 @@ export PATH=$(get_abs_filename "./${PREFIX_DIR}")/bin:${PATH}
 echo "PATH :" $PATH | grep -i idstools
 
 echo "Tools testing with testscripts with default IMAS Access Layer"
-chmod +x ./tests/testscripts.sh
-try source ./tests/st01_test_ids_scripts.sh || exit 1
-try source ./tests/st02_test_db_scripts.sh || exit 1
-try source ./tests/st03_test_analysis_scripts.sh || exit 1
+chmod +x ./tests/st01_test_ids_scripts.sh
+chmod +x ./tests/st02_test_db_scripts.sh
+chmod +x ./tests/st03_test_analysis_scripts.sh
+try source ./tests/st01_test_ids_scripts.sh 
+try source ./tests/st02_test_db_scripts.sh 
+try source ./tests/st03_test_analysis_scripts.sh 
 
 echo "-------------------------------------------------------------------------"
 echo "Tools testing with testscripts with IMAS Access Layer 5"
@@ -42,9 +44,9 @@ echo "PYTHONPATH :" $PYTHONPATH | grep -i idstools
 export PATH=$(get_abs_filename "./${PREFIX_DIR}")/bin:${PATH}
 echo "PATH :" $PATH | grep -i idstools
 
-try source ./tests/st01_test_ids_scripts.sh || exit 1
-try source ./tests/st02_test_db_scripts.sh || exit 1
-try source ./tests/st03_test_analysis_scripts.sh || exit 1
+try source ./tests/st01_test_ids_scripts.sh 
+try source ./tests/st02_test_db_scripts.sh 
+try source ./tests/st03_test_analysis_scripts.sh 
 
 echo "Run pytest for functions testing"
 try python3 -m pytest --junit-xml=${PREFIX_DIR}/test_report.xml tests || exit 1
