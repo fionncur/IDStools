bb013f3e0ff8fd104e33c783d28efa8e7857cb37
Sawantdesai Prasad parshuram EXT <Prasadparshuram.Sawantdesai@iter.org>
2024-09-29 23:03:16 +0200

    fix merge issues

--- tests/st01_test_ids_scripts_with_uri.sh
+++ tests/st01_test_ids_scripts_with_uri.sh
@@ -27,14 +27,22 @@ else
 fi
 
 SCRIPTS=(
-    "eqdsk2ids -c 11 --src \"imas:mdsplus?user=public;pulse=134174;run=117;database=ITER;version=3\" -g resources/geqdsk/example.gfile --dest \"imas:mdsplus?user=$USERNAME;pulse=134174;run=117;database=ITER;version=3?path=$DATABASE_DIR\" --log INFO"
+    "eqdsk2ids -c 11 --src \"imas:mdsplus?user=public;pulse=134174;run=117;database=ITER;version=3\" -g resources/geqdsk/example.gfile --dest \"imas:hdf5?user=$USERNAME;pulse=134174;run=117;database=ITER;version=3?path=$DATABASE_DIR\" --log INFO"
     "idschk --uri \"imas:mdsplus?user=public;pulse=134174;run=117;database=ITER;version=3\" -f resources/validation_schemas/generic/core_profiles.yml"
     "idscp --src \"imas:mdsplus?user=public;pulse=131024;run=10;database=ITER;version=3\" --dest \"imas:hdf5?user=$USERNAME;pulse=145000;run=5;database=ITER;version=3?path=$DATABASE_DIR\""
+<<<<<<< HEAD
     "idsdiff --ids summary --uri \"imas:mdsplus?user=public;pulse=122525;run=1;database=ITER;version=3\" \"imas:mdsplus?user=public;pulse=122525;run=2;database=ITER;version=3\""
     "idsdiff --ids summary --uri \"imas:mdsplus?user=public;pulse=130011;run=6;database=ITER;version=3\" \"imas:mdsplus?user=public;pulse=130012;run=4;database=ITER;version=3\""
     "idsresample --src \"imas:mdsplus?user=public;pulse=131024;run=10;database=ITER;version=3\" --dest \"imas:mdsplus?user=$USERNAME;pulse=131024;run=5;database=ITER;version=3?path=$DATABASE_DIR\""
     "idsrescale_equilibrium --src \"imas:mdsplus?user=public;pulse=122525;run=1;database=ITER;version=3\" --dest \"imas:mdsplus?user=$USERNAME;pulse=122222;run=22;database=ITER;version=3?path=$DATABASE_DIR\"  --rescale 2"
     "idsshift_equilibrium --src \"imas:mdsplus?user=public;pulse=122525;run=1;database=ITER;version=3\" --dest \"imas:mdsplus?user=$USERNAME;pulse=123001;run=1;database=ITER;version=3?path=$DATABASE_DIR\"  --shift -0.01"
+=======
+    "idsdiff summary --uri \"imas:mdsplus?user=public;pulse=122525;run=1;database=ITER;version=3\" \"imas:mdsplus?user=public;pulse=122525;run=2;database=ITER;version=3\""
+    "idsdiff summary --uri \"imas:mdsplus?user=public;pulse=130011;run=6;database=ITER;version=3\" \"imas:mdsplus?user=public;pulse=130012;run=4;database=ITER;version=3\""
+    "idsresample --src \"imas:mdsplus?user=public;pulse=131024;run=10;database=ITER;version=3\" --dest \"imas:hdf5?user=$USERNAME;pulse=131024;run=5;database=ITER;version=3?path=$DATABASE_DIR\""
+    "idsrescale_equilibrium --src \"imas:mdsplus?user=public;pulse=122525;run=1;database=ITER;version=3\" --dest \"imas:hdf5?user=$USERNAME;pulse=122222;run=22;database=ITER;version=3?path=$DATABASE_DIR\"  --rescale 2"
+    "idsshift_equilibrium --src \"imas:mdsplus?user=public;pulse=122525;run=1;database=ITER;version=3\" --dest \"imas:hdf5?user=$USERNAME;pulse=123001;run=1;database=ITER;version=3?path=$DATABASE_DIR\"  --shift -0.01"
+>>>>>>> aeadbe6 (change dest to hdf5 backend)
     "idslist --uri \"imas:mdsplus?user=public;pulse=122525;run=1;database=ITER;version=3\""
     "idslist --uri \"imas:mdsplus?user=public;pulse=122525;run=1;database=ITER;version=3\" -y"
     "idslist --uri \"imas:mdsplus?user=public;pulse=122525;run=1;database=ITER;version=3\" -c"
