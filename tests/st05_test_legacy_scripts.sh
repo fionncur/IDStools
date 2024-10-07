13490f6b29d16fc3b7179d5d43f7c0569854ef90
Sawantdesai Prasad parshuram EXT <Prasadparshuram.Sawantdesai@iter.org>
2024-10-07 17:26:55 +0200

    added missing file

---
+++ tests/st05_test_legacy_scripts.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Bamboo test execution script for analysis scripts
+# Execute script from root directory
+source ./tests/st00_common.sh
+# expand aliases
+shopt -p expand_aliases
+
+#print hostname
+hostname -f
+
+# create log directory
+if [ -z "$1" ]; then
+    LOG_DIR=$PWD/"logs"
+    mkdir -p "$LOG_DIR"
+else
+    LOG_DIR="$1"
+fi
+
+if [ -z "$2" ]; then
+    DATABASE_DIR=$PWD/"db"
+    mkdir -p "$DATABASE_DIR"
+else
+    DATABASE_DIR="$2"
+fi
+
+SCRIPTS=(
+    "eqdsk2ids -c 11 --src \"imas:mdsplus?user=public;pulse=134174;run=117;database=ITER;version=3\" -g resources/geqdsk/example.gfile --dest \"imas:hdf5?user=$USERNAME;pulse=134174;run=117;database=ITER;version=3?path=$DATABASE_DIR\" --log INFO"
+    "eqdsk2ids -c 11 --src \"imas:mdsplus?user=public;pulse=134174;run=117;database=ITER;version=3\" -g resources/geqdsk/example.gfile --dest \"imas:hdf5?user=$USERNAME;pulse=134174;run=117;database=ITER;version=3?path=$DATABASE_DIR\" --log INFO"
+    "idschk --uri \"imas:mdsplus?user=public;pulse=134174;run=117;database=ITER;version=3\" -f resources/validation_schemas/generic/core_profiles.yml"
+    "create_db_entry -s 130012 -r 105 -d TEST --disable-validation"
+    "create_db_entry_disruption -s 100028 -r 1 -d ITER_DISRUPTIONS"
+    "create_validation_schema -i core_profiles"
+    "disruption_summary"
+    "validate_db_entry -s 134174 -r 117 --path resources/validation_schemas")
+
+execute_scripts "${SCRIPTS[@]}"
+STATUS=$?
+if [[ "$(uname -n)" == *"bamboo"* ]]; then
+    if [ "$STATUS" -ne 0 ]; then
+        return "$STATUS"
+    fi
+fi
